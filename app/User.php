f1ca81d4c63e21b4e8a7fc15044625b13c05906b
Felipe Henrique <49046577+FelipeSHenrique@users.noreply.github.com>
2020-12-22 20:43:02 -0300

    first commit

---
+++ app/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    public $timestamps = false;
+
+    protected $hidden = ['password', 'token'];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+}
