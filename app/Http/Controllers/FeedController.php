ddfc3d9eff7efa26f5a441768ff6f64537c024aa
Felipe Henrique <49046577+FelipeSHenrique@users.noreply.github.com>
2020-12-25 13:29:20 -0300

    Adicionado funcao de cadastro de post e exibir post

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -4,6 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Post;
+use App\PostLike;
+use App\PostComment;
+use App\UserRelation;
+use App\User;
+use Image;
 
 class FeedController extends Controller
 {
@@ -12,5 +18,154 @@ class FeedController extends Controller
     public function __construct() {
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
+    }
+
+    public function create(Request $request) {
+        $array = ['error'=>''];
+        // Formatos de imagens que o sistema aceita.
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        $type = $request->input('type');
+        $body = $request->input('body');
+        $photo = $request->file('photo');
+
+        if ($type) {
+
+            switch($type) {
+                case 'text':
+                    // Verifica se existe um body
+                    if (!$body) {
+                        $array['error'] = 'Texto não enviado';
+                        return $array;
+                    }
+                break;
+                case 'photo':
+                    if ($photo) {
+
+                        if (in_array($photo->getClientMimeType(), $allowedTypes)) {
+
+                            $filename = md5(time().rand(0,9999)).'.jpg';
+
+                            $destPath = public_path('/media/uploads');
+
+                            $img = Image::make($photo->path())
+                                // Imagem vai ter no maximo 800 de largura e altura vai ser proporcional
+                                ->resize(800, null, function($constraint) {
+                                    // constraint = Vai fazer com que minha imagem mantenha a proporção
+                                    $constraint->aspectRatio();
+                                })
+                                ->save($destPath.'/'.$filename);
+
+                            $body = $filename;    
+
+                        } else {    
+                            $array['error'] = 'Arquivo não suportado';
+                            return $array;
+                        }
+
+                    } else {
+                        $array['error'] = 'Arquivo não enviado';
+                        return $array;
+                    }
+                break;
+                default:
+                    $array['error'] = 'Tipo de postagem inexistente.';
+                    return $array;
+                break;
+            }
+
+            if ($body) {
+                $newPost = new Post();
+                $newPost->id_user = $this->loggedUser['id'];
+                $newPost->type = $type;
+                $newPost->created_at = date('Y-m-d H:i:s');
+                $newPost->body = $body;
+                $newPost->save();
+            }
+
+        } else {
+            $array['error'] = 'Dados não enviados';
+            return $array;
+        }
+
+        return $array;
+    }
+
+    public function read(Request $request) {
+        $array = ['error'=>''];
+
+        // Se o usuário não mandar nada o page vai ser ZERO
+        $page = intval($request->input('page'));
+        // Quantos post por página
+        $perPage = 2;
+
+        // 1. Pegar a lista de usuários que EU sigo (Incluindo EU mesmo)
+        $users = [];
+        $userList = UserRelation::where('user_from', $this->loggedUser['id'])->get();
+        foreach($userList as $userItem) {
+            $users[] = $userItem['user_to'];
+        }
+        $users[] = $this->loggedUser['id'];
+
+        // 2. Pegar os posts dessa galera ORDENADO PELA DATA
+        $postList = Post::whereIn('id_user', $users)
+        ->orderBy('created_at', 'desc')
+        ->offset($page * $perPage)
+        ->limit($perPage)
+        ->get();
+
+        // Pega o total de post
+        $total = Post::whereIn('id_user', $users)->count();
+        $pageCount = ceil($total / $perPage);
+
+        // 3. Preencher as informações adicionais
+        $posts = $this->_postListToObject($postList, $this->loggedUser['id']);
+
+        $array['posts'] = $posts;
+        $array['pageCount'] = $pageCount;
+        $array['currentPage'] = $page;
+
+        return $array;
+    }
+
+    private function _postListToObject($postList, $loggedId) {
+        foreach($postList as $postKey => $postItem) {
+
+            // Verifica se o post é meu
+            if ($postItem['id_user'] == $loggedId) {
+                $postList[$postKey]['mine'] = true;
+            } else {
+                $postList[$postKey]['mine'] = false;
+            }
+
+            // Preencher informações de usuário
+            $userInfo = User::find($postItem['id_user']);
+            $userInfo['avatar'] = url('media/avatars/'.$userInfo['avatar']);
+            $userInfo['cover'] = url('media/covers/'.$userInfo['cover']);
+            $postList[$postKey]['user'] = $userInfo;
+
+            // Preencher informações de LIKE
+            $likes = PostLike::where('id_post', $postItem['id'])->count();
+            $postList[$postKey]['likeCount'] = $likes;
+
+            // Verifica se eu curti o Post
+            $isLiked = PostLike::where('id_post', $postItem['id'])
+            ->where('id_user', $loggedId)
+            ->count();
+            $postList[$postKey]['liked'] = ($isLiked > 0) ? true : false;
+
+            // Preencher informações de COMMENTS
+            $comments = PostComment::where('id_post', $postItem['id'])->get();
+            foreach($comments as $commentKey => $comment) {
+                $user = User::find($comment['id_user']);
+                $user['avatar'] = url('media/avatars/'.$user['avatar']);
+                $user['cover'] = url('media/covers/'.$user['cover']);
+                $comments[$commentKey]['user'] = $user;
+            }
+            $postList[$postKey]['comments'] = $comments;
+
+        }
+
+        return $postList;
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\User;
+// Biblioteca de imagens
+use Image;
 
 class UserController extends Controller
 {
@@ -12,5 +15,154 @@ class UserController extends Controller
     public function __construct() {
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
+    }
+
+    public function update(Request $request) {
+        $array = ['error' => ''];
+
+        $name = $request->input('name'); 
+        $email = $request->input('email');
+        $birthdate = $request->input('birthdate');
+        $city = $request->input('city');
+        $work = $request->input('work');
+        $password = $request->input('password');
+        $password_confirm = $request->input('password_confirm');
+
+        $user = User::find($this->loggedUser['id']);
+
+        // NAME
+        if ($name) {
+            $user->name = $name;
+        }
+
+        // E-MAIL
+        if ($email) {
+            if ($email != $user->email) {
+                $emailExists = User::where('email', $email)->count();
+                if ($emailExists === 0) {
+                    $user->email = $email;
+                } else {
+                    $array['error'] = 'E-mail já existe!';
+                    return $array;
+                }
+            }
+        }
+
+        // BIRTHDATE
+        if ($birthdate) {
+            if (strtotime($birthdate) === false) {
+                $array['error'] = 'Data de nascimento inválida';
+                return $array;
+            }
+            $user->birthdate = $birthdate;
+        }
+
+        // CITY
+        if ($city) {
+            $user->city = $city;
+        }
+
+        //WORK
+        if ($work) {
+            $user->work = $work;
+        }
+
+        // PASSWORD
+        if ($password && $password_confirm) {
+            if ($password === $password_confirm) {
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $user->password = $hash;
+            } else {
+                $array['error'] = 'As senhas não batem.';
+                return $array;
+            }
+        }
+
+        $user->save();
+        
+        return $array;
+    }
+
+    public function updateAvatar(Request $request) {
+        $array = ['error'=>''];
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        $image = $request->file('avatar');
+
+        if ($image) {
+            // getClientMimeType = Verifica qual o tipo do arquivo (https://www.php.net/manual/en/function.mime-content-type.php)
+            // Verificando se o arquivo é do tipo jpg/jpeg/png
+            if (in_array($image->getClientMimeType(), $allowedTypes)) {
+                
+                //Gerar nome aleatorio para o arquivo
+                $filename = md5(time().rand(0,9999)).'.jpg';
+
+                // Arquivo que vou salvar a imagem
+                $destPath = public_path('/media/avatars');
+
+                // Mando o arquivo original que fiz o upload
+                $img = Image::make($image->path())
+                // Tamanho da imagem
+                    ->fit(200, 200)    
+                    ->save($destPath.'/'.$filename);
+
+                $user = User::find($this->loggedUser['id']);
+                $user->avatar = $filename;
+                $user->save();
+                
+                $array['url'] = url('/media/avatars/'.$filename);
+
+            } else {
+                $array['error'] = 'Arquivo não suportado!';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Arquivo não enviado!';
+            return $array;
+        }
+
+        return $array;
+    }
+
+    public function updateCover(Request $request) {
+        $array = ['error'=>''];
+        // Formatos de imagens que o sistema aceita.
+        $allowedTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+
+        $image = $request->file('cover');
+
+        if ($image) {
+            // getClientMimeType = Verifica qual o tipo do arquivo (https://www.php.net/manual/en/function.mime-content-type.php)
+            // Verificando se o arquivo é do tipo jpg/jpeg/png
+            if (in_array($image->getClientMimeType(), $allowedTypes)) {
+                
+                //Gerar nome aleatorio para o arquivo
+                $filename = md5(time().rand(0,9999)).'.jpg';
+
+                // Arquivo que vou salvar a imagem
+                $destPath = public_path('/media/covers');
+
+                // Mando o arquivo original que fiz o upload
+                $img = Image::make($image->path())
+                // Tamanho da imagem
+                    ->fit(850, 310)    
+                    ->save($destPath.'/'.$filename);
+
+                $user = User::find($this->loggedUser['id']);
+                $user->cover = $filename;
+                $user->save();
+                
+                $array['url'] = url('/media/covers/'.$filename);
+
+            } else {
+                $array['error'] = 'Arquivo não suportado!';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Arquivo não enviado!';
+            return $array;
+        }
+
+        return $array;
     }
 }
